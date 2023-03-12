99a3de7ff0a018f0519a3a7d48e920f3fb2542dc
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-12 15:31:34 +0200

    My third commit Module4.1 Task3

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -1,10 +1,14 @@
 package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
+    public static void main(String[] args) {
+        new NinesSum().calculateSum(0);
+    }
     public void calculateSum(int lengthOfLastNumber) {
         int result = 0;
         if (lengthOfLastNumber == 0){
             System.out.println(0);
+            return;
         } else if (lengthOfLastNumber == 1){
             System.out.println(9);
         } else {
